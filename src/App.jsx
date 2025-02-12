2051fc63888f11477ab602eaca9b5fa2e470a4a2
Carlos Barrios <cabarriosb@gmail.com>
2025-02-12 12:11:45 -0500

    use Effect}

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,77 @@
-import './App.css'
-import Navbar from './Navbar'
-import Body from './Body'
-import Footer from './Footer'
-import { useState } from 'react'
+import "./App.css";
+import Navbar from "./Navbar";
+import Body from "./Body";
+import Footer from "./Footer";
+import { useEffect, useState } from "react";
 
-const libros = [
-  { name: "El principito", description: "Un clásico de la literatura que narra la historia de un pequeño príncipe y sus reflexiones sobre la vida y la amistad.", price: 9.99, isStarred: false },
-  { name: "1984", description: "Una novela distópica de George Orwell que explora los peligros del totalitarismo y la vigilancia extrema.", price: 12.50, isStarred: false },
-  { name: "Cien años de soledad", description: "La obra maestra de Gabriel García Márquez, que narra la historia de la familia Buendía en el pueblo de Macondo.", price: 14.99, isStarred: false },
-  { name: "Don Quijote", description: "La novela de Miguel de Cervantes que sigue las aventuras de un caballero idealista y su fiel escudero, Sancho Panza.", price: 19.99, isStarred: false },
-  { name: "Orgullo y prejuicio", description: "Una novela de Jane Austen que explora el amor, la clase social y los malentendidos en la Inglaterra del siglo XIX.", price: 11.75, isStarred: false },
-  { name: "Matar a un ruiseñor", description: "Un libro de Harper Lee que trata sobre la justicia y el racismo en el sur de los Estados Unidos.", price: 13.25, isStarred: false },
-  { name: "Crimen y castigo", description: "Un clásico de Dostoievski que sigue la lucha moral de un joven después de cometer un crimen.", price: 15.50, isStarred: false },
-  { name: "Los juegos del hambre", description: "Una novela distópica de Suzanne Collins sobre una sociedad opresiva y un torneo mortal.", price: 10.99, isStarred: false },
-  { name: "La metamorfosis", description: "Una novela de Franz Kafka que narra la historia de un hombre que se despierta transformado en un insecto gigante.", price: 8.99, isStarred: false },
-  { name: "Dorian Gray", description: "Una novela de Oscar Wilde que explora la moralidad y la decadencia a través de la historia de un joven que no envejece.", price: 11.50, isStarred: false },
-  { name: "Fahrenheit 451", description: "Una novela de Ray Bradbury que describe una sociedad futura donde los libros están prohibidos y son quemados.", price: 10.75, isStarred: false },
-  { name: "El guardián", description: "Una novela de J.D. Salinger que sigue las experiencias de un adolescente rebelde en Nueva York.", price: 9.50, isStarred: false }
-];
 
+async function fetchApi(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data", error);
+  }
+}
 
 function App() {
-  const [search,setSearch] = useState("");
-  const [books,setBooks] = useState(libros);
+  const [search, setSearch] = useState("");
+  const [products, setProducts] = useState([]);
+
+  console.log("Productos", products);
 
   function handleSearchChange(e) {
     setSearch(e.target.value);
   }
 
-  function handleFavorite(name) {
-    const newBooks = books.map((book) => {
-      if(book.name === name){
-        return {...book,
-          isStarred:!book.isStarred}
+  function handleFavorite(id) {
+    const newProducts = products.map((product) => {
+      if (product.id === id) {
+        return { ...product, isStarred: !product.isStarred };
       }
-      return book
+      return product;
     });
-    
-    setBooks(newBooks);   
+
+    setProducts(newProducts);
   }
+
+  useEffect(() => {
+    const initProducts = async () => {
+      
+      const data = await fetchApi("https://fakestoreapi.com/products");
+      const newProducts = data.map((p) => {
+        return {
+          ...p,
+          isStarred: false,
+        };
+      });
+      setProducts(newProducts);
+
+    };
+
+    initProducts();
+  }, []);
+
+  //loadSkeletons -> Cargar Esqueletos
+  //Grid - Flex -> Importancia en el espacio a disponer
 
   return (
     <>
-    <div>
-      <Navbar search={search} handleSearchChange={handleSearchChange}></Navbar>
-      <Body books={books} search={search} handleFavorite={handleFavorite}></Body>
-      <Footer></Footer>
-    </div>
+      <div>
+        <Navbar
+          search={search}
+          handleSearchChange={handleSearchChange}
+        ></Navbar>
+        <Body
+          products={products}
+          search={search}
+          handleFavorite={handleFavorite}
+        ></Body>
+        <Footer></Footer>
+      </div>
     </>
-  ) 
+  );
 }
 
 export default App;
--- src/Body.jsx
+++ src/Body.jsx
@@ -1,16 +1,16 @@
 
-function Body({ books, search, handleFavorite }) {
+function Body({ products, search, handleFavorite }) {
 
-  const filteredBooks = books.filter((book) =>
-    book.name.toLowerCase().includes(search.toLowerCase())
+  const filteredProducts = products.filter((product) =>
+    product.title.toLowerCase().includes(search.toLowerCase())
   );
 
   return (
     <div className="flex flex-wrap justify-center gap-6 p-8">
-      {filteredBooks.map((book) => (
+      {filteredProducts.map((product) => (
         <Card
-          key={book.name + book.price}
-          book={book}
+          key={product.id}
+          product={product}
           handleFavorite={handleFavorite}
         ></Card>
       ))}
@@ -18,24 +18,24 @@ function Body({ books, search, handleFavorite }) {
   );
 }
 
-function Card({ book, handleFavorite }) {
+function Card({ product, handleFavorite }) {
 
-  const precioType = "$" + book.price;
+  const precioType = "$" + product.price;
 
 
 
   return (
     <div className={`flex flex-col bg-gray-200 p-4 gap-4 w-48 h-64 rounded-sm`}>
       <div className="flex flex-wrap justify-center items-center h-12 w-full rounded-sm bg-gray-400">
-        <p className="text-sm">{book.name}</p>
+        <p className="text-sm">{product.title.slice(0,10)}</p>
       </div>
 
       <div className="relative flex flex-col justify-between h-full">
-        <p className="text-xs">{book.description}</p>
+        <p className="text-xs">{product.description.slice(0,90)}</p>
         <span className="self-end text-xs">{precioType}</span>
-        <button onClick={()=>handleFavorite(book.name)}
+        <button onClick={()=>handleFavorite(product.id)}
         className="absolute bottom-0 text-yellow-500">
-          {book.isStarred ? "★" : "☆"}
+          {product.isStarred ? "★" : "☆"}
         </button>
       </div>
     </div>
